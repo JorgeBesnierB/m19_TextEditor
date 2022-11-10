1950267bd3d931dadf932d61d8f8de2348d6159a
Jorge Besnier <jorgebesnierb@outlook.com>
2022-11-09 19:28:24 -0600

    wrong repo files

--- docs/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require ("express").Router();
-// Import API routes from /api/index.js 
-const apiRoutes = require("./api");
-// "/api" prefix added to all api routes imported from the "api" directory
-router.use("/api", apiRoutes);
-
-module.exports = router;
